575bc1dbd4c98d70e99e7978710f7aebf45a79b9
holasoypasha <klaviatura566@gmail.com>
2025-06-30 19:03:01 +0300

    complete program for creating a family

--- src/ru/pavlova/createFamily/AnimalType.java
+++ src/ru/pavlova/createFamily/AnimalType.java
@@ -1,4 +1,21 @@
 package ru.pavlova.createFamily;
 
+/**
+ * Перечисление возможных типов животных
+ */
 public enum AnimalType {
+    CAT("Кот"),
+    DOG("Собака"),
+    BIRD("Птичка"),
+    HAMSTER("Хомяк");
+
+    private String animalName;
+
+    AnimalType(String rusName) {
+        this.animalName = rusName;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
 }
--- src/ru/pavlova/createFamily/Human.java
+++ src/ru/pavlova/createFamily/Human.java
@@ -1,4 +1,43 @@
 package ru.pavlova.createFamily;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Human {
+    /**
+     * Возраст человека
+     */
+    private int age;
+    /**
+     * Имя человека
+     */
+    private String name;
+    /**
+     * Список животных
+     */
+    private List<Animal> animals;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.animals = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+        animal.setOwner(this);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Возраст: " + age + ")";
+    }
 }
